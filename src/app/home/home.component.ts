9cf0493409fcc650e1e9dc26b21a5633b344525c
Armando <rivera.armando997@gmail.com>
2020-08-26 21:19:25 -0500

    Implementando swiper para un banner en el home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  public mySwiper: Swiper;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    // Indicamos que inicialize este plugin propio de swiper cuando termine de cargar la vista
+    this.mySwiper = new Swiper('.swiper-container');
+  }
 }
