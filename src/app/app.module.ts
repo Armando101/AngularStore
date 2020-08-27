9cbec8e5cf5e8c1eb72ea097066de37d4fd39a44
Armando <rivera.armando997@gmail.com>
2020-08-26 20:41:40 -0500

    Creando una directiva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 
 import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    CartComponent
+    CartComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
