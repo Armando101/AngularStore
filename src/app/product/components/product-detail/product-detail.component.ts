4b5329773b570095f41494026d31636f2b2b0f2f
Armando <rivera.armando997@gmail.com>
2020-08-29 20:31:19 -0500

    Create and update product

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -21,8 +21,37 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      this.product = this.productsService.getProduct(id);
+      this.fetchProduct(id);
     });
   }
 
+  fetchProduct(id: string): void {
+    this.productsService.getProduct(id)
+      .subscribe((product: Product) => {
+        this.product = product;
+      });
+  }
+
+  createProduct(): void {
+    const newProduct: Product = {
+      id: '222',
+      title: 'My product',
+      image: 'assets/images/banner-1.png',
+      price: 300,
+      description: 'New Product'
+    };
+
+    this.productsService.createProduct(newProduct)
+      .subscribe(product => {
+        console.log(product);
+      });
+  }
+
+  updateProduct(id: string): void {
+    const newProduct = {};
+    this.productsService.updateProduct(id, newProduct)
+      .subscribe(product => {
+        console.log(product);
+      });
+  }
 }
