444088f4064258eb79a90f6fbe94fc12c805d938
Armando <rivera.armando997@gmail.com>
2020-09-02 17:03:16 -0500

    Observables con async

--- src/app/contact/components/layout/layout.component.ts
+++ src/app/contact/components/layout/layout.component.ts
@@ -4,6 +4,8 @@ import { GeneratorService } from '@core/services/generator.service';
 import { EmployeeData } from '@core/models/employee.model';
 import { Observable, Subscription } from 'rxjs';
 
+import { tap } from 'rxjs/operators';
+
 const names = ['Armando', 'Rodrigo', 'Jenny', 'Yeled', 'Mauricio', 'Juan'];
 
 @Component({
@@ -16,28 +18,35 @@ export class LayoutComponent implements OnInit, OnDestroy {
   public salesList: EmployeeData[] = [];
   public bList: EmployeeData[] = [];
 
+  public value$: Observable<number>;
+
   public value: number;
 
-  public sub$: Subscription;
+  // public sub$: Subscription;
 
   constructor(
     private generatorService: GeneratorService
-  ) { }
+  ) {
+    this.value$ = this.generatorService.getData()
+      .pipe(
+        tap(num => console.log(num))
+      );
+  }
 
   ngOnInit(): void {
     this.salesList = this.generatorService.generate(names, [10, 20], 10);
     this.bList = this.generatorService.generate(names, [10, 20], 10);
 
-    this.sub$ = this.generatorService.getData()
-      .subscribe(value => {
-        this.value = value;
-        console.log(value);
-      });
+    // this.sub$ = this.generatorService.getData()
+    //   .subscribe(value => {
+    //     this.value = value;
+    //     console.log(value);
+    //   });
   }
 
   ngOnDestroy(): void {
-    console.log('Destroy');
-    this.sub$.unsubscribe();
+    // console.log('Destroy');
+    // this.sub$.unsubscribe();
   }
 
   addItem(list: EmployeeData[], label: string): void {
