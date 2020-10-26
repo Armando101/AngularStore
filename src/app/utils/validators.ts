4a0ceef1941eaec27271e926f64b9afa3aa22a39
Armando <rivera.armando997@gmail.com>
2020-10-25 21:34:44 -0600

    Forms -Validaciones personalizadas

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -8,4 +8,20 @@ export class MyValidators {
         }
         return null;
     }
+
+    static validPassword(control: AbstractControl): object {
+        const value = control.value;
+        if (!containsNumber(value)) {
+            return {invalid_password: true};
+        }
+        return null;
+    }
+}
+
+function isNumber(value: string): boolean {
+    return !isNaN(parseInt(value, 10));
+}
+
+function containsNumber(value: string): boolean {
+    return value.split('').find(v => isNumber(v)) !== undefined;
 }
